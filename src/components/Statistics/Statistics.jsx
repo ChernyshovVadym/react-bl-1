c36a9734b1e186acb5d6fac9da304852a52bf169
OleksaSnuff <alex1204park@gmail.com>
2024-03-16 13:29:22 +0200

    upd

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,31 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+const icons = [
+  <FaRegThumbsUp key={0} />,
+  <MdPeople key={1} />,
+  <MdOutlineProductionQuantityLimits key={2} />,
+  <GiTreeDoor key={3} />,
+];
+
+export const Statistics = ({ title, stats }) => {
+  //const desc = <h2 className={style.title}>{title}</h2>;
+  return (
+    <>
+      {title && <h2 className={style.title}>{title}</h2>}
+      <ul className={style.list}>
+        {stats.map(({ id, title, total }, index) => (
+          <StatisticsItem
+            key={id}
+            title={title}
+            total={total}
+            icon={icons[index]}
+          />
+        ))}
+      </ul>
+    </>
+  );
 };
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,14 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import styled from './Statistics.module.css';
+import { IconContext } from 'react-icons';
+
+export const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <li className={styled.item}>
+      <IconContext.Provider value={{ color: '#29a14f', size: 30 }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={styled.counter}>{total}</span>
+      <p className={styled.text}>{title}</p>
+    </li>
+  );
 };
